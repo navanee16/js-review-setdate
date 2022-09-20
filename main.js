ece990ff14a37374af785185c9720b6469c994ad
Navanee <navaneekrish164@gmail.com>
2022-09-20 15:49:23 +0530

    js

---
+++ main.js
@@ -0,0 +1,52 @@
+const ramani =new Date();
+ ramani.setDate(15);
+console.log(ramani);
+
+
+var ramanis= new Date();
+ramanis.setHours(23);
+console.log(ramanis);
+
+var x = new Date();
+x.setMinutes(40);
+console.log(x);
+
+
+var mani = new Date();
+mani.setSeconds(20);
+console.log(mani);                                                  
+                                                                  
+var y =new Date();
+y.setMilliseconds(30);
+console.log(y);
+
+var z = new Date();
+z.setMonth(8);
+console.log(z);
+
+var t =ramani.getHours();
+console.log(t);
+
+var h =ramani.getMinutes();
+console.log(h);
+
+var tear = ramani.getFullYear();
+console.log(tear);
+
+var year = ramani.getYear();
+console.log(year);
+
+var y =ramani.getUTCDate();
+console.log(y);
+
+var s= ramani.getUTCHours();
+console.log(s);
+
+var m=ramani.getUTCMinutes();
+console.log(m);
+
+var us=ramani.getUTCSeconds();
+console.log(us);
+
+var ml=ramani.getUTCMilliseconds();
+console.log(ml);
